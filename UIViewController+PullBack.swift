a3bf98f11cb074a8a602c88f3036ba49b066113e
梁大红 <liangdahong@bluemoon.com.cn>
2016-08-17 11:28:26 +0800

    add swift

---
+++ UIViewController+PullBack.swift
@@ -0,0 +1,45 @@
+//
+//  UIViewController+PullBack.swift
+//  PullBackSwift
+//
+//  Created by ___liangdahong on 16/8/17.
+//  Copyright © 2016年 梁大红. All rights reserved.
+//
+
+/// false: 关闭 true: 打开
+
+let kopenPullBack = true
+
+import UIKit
+extension UIViewController {
+    public override class func initialize() {
+
+        struct Static {
+            static var token: dispatch_once_t = 0
+        }
+        if self !== UIViewController.self {
+            return
+        }
+        dispatch_once(&Static.token) {
+            if kopenPullBack {
+                print("dispatch_once")
+                let m1 = class_getInstanceMethod(self, #selector(viewDidLoad))
+                let m2 = class_getInstanceMethod(self, #selector(bm_viewDidLoad))
+                method_exchangeImplementations(m1, m2)
+            }
+        }
+    }
+
+    func bm_viewDidLoad() -> () {
+        if self.isKindOfClass(UINavigationController) {
+            let tempGes = (self as! UINavigationController).interactivePopGestureRecognizer
+            tempGes?.enabled = false
+            let arr = tempGes?.valueForKey("_targets")
+            let tar = arr?.firstObject
+            let _targets = tar!!.valueForKey("target");
+            let sel = NSSelectorFromString("handleNavigationTransition:")
+            let pan = UIPanGestureRecognizer.init(target: _targets!, action: sel)
+            tempGes!.view?.addGestureRecognizer(pan)
+        }
+    }
+}
