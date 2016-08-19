2314b6ed0e612916464c1807c0dfc55175339b48
梁大红 <liangdahong@bluemoon.com.cn>
2016-08-19 11:59:52 +0800

    修改一个bug

--- OCDemo/UIViewController+BMBack/UIViewController+BMBack.m
+++ OCDemo/UIViewController+BMBack/UIViewController+BMBack.m
@@ -11,32 +11,41 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@implementation UIViewController (BMBack)
+#if kopenPullBack
+@implementation UINavigationController (BMBack)
 
 + (void)load {
-    if (kopenPullBack) {
-        Method m1 = class_getInstanceMethod([self class], @selector(viewDidLoad));
-        Method m2 = class_getInstanceMethod([self class], @selector(bm_viewDidLoad));
+    Method m1 = class_getInstanceMethod([self class], @selector(viewDidLoad));
+    Method m2 = class_getInstanceMethod([self class], @selector(bm_viewDidLoad));
+    
+    BOOL sel = class_addMethod(self, @selector(viewDidLoad), method_getImplementation(m2), method_getTypeEncoding(m2));
+    
+    if (!sel) {
+        // 添加成功 说明本类已经实现了方法 交换即可
         method_exchangeImplementations(m1, m2);
+    }else{
+        // 添加成功 就替换掉方法
+        class_replaceMethod(self, @selector(bm_viewDidLoad), method_getImplementation(m1), method_getTypeEncoding(m2));
     }
 }
 
 - (void)bm_viewDidLoad {
     [self bm_viewDidLoad];
-    if ([self isKindOfClass:[UINavigationController class]]) {
-        // 获取手势
-        UIGestureRecognizer *tempGes = ((UINavigationController *)self).interactivePopGestureRecognizer;
-        // 关闭此手势
-        tempGes.enabled = NO;
-        // 获取手势的回调数组
-        NSMutableArray *muarray = [tempGes valueForKey:@"_targets"];
-        
-        // 获取系统的侧滑手势的回调对象和方法
-        id tar = [[muarray firstObject] valueForKey:@"target"];
-        SEL sel = NSSelectorFromString(@"handleNavigationTransition:");
-        // 创建一个手势 添加上去
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:tar action:sel];
-        [tempGes.view addGestureRecognizer:pan];
-    }
+    
+    // 获取手势
+    UIGestureRecognizer *tempGes = self.interactivePopGestureRecognizer;
+    // 关闭此手势
+    tempGes.enabled = NO;
+    // 获取手势的回调数组
+    NSMutableArray *muarray = [tempGes valueForKey:@"_targets"];
+    
+    // 获取系统的侧滑手势的回调对象和方法
+    id tar = [[muarray firstObject] valueForKey:@"target"];
+    SEL sel = NSSelectorFromString(@"handleNavigationTransition:");
+    // 创建一个手势 添加上去
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:tar action:sel];
+    [tempGes.view addGestureRecognizer:pan];
 }
 @end
+#endif
+
--- SwiftDemo/UIViewController+PullBack/UIViewController+PullBack.swift
+++ SwiftDemo/UIViewController+PullBack/UIViewController+PullBack.swift
@@ -7,39 +7,43 @@
 //  https://github.com/asiosldh/PullBack
 
 /// false: 关闭 true: 打开
-
 let kopenPullBack = true
+
 import UIKit
 
-extension UIViewController {
+extension UINavigationController {
     public override class func initialize() {
-
+        
         struct Static {
             static var token: dispatch_once_t = 0
         }
-        if self !== UIViewController.self {
+        if self !== UINavigationController.self {
             return
         }
         dispatch_once(&Static.token) {
             if kopenPullBack {
-                print("dispatch_once")
                 let m1 = class_getInstanceMethod(self, #selector(viewDidLoad))
                 let m2 = class_getInstanceMethod(self, #selector(bm_viewDidLoad))
-                method_exchangeImplementations(m1, m2)
+                
+                let sel = class_addMethod(self, #selector(viewDidLoad), method_getImplementation(m2), method_getTypeEncoding(m2))
+                if sel {
+                    class_replaceMethod(self, #selector(bm_viewDidLoad), method_getImplementation(m1), method_getTypeEncoding(m1))
+                }else{
+                    method_exchangeImplementations(m1, m2)
+                }
             }
         }
     }
 
     func bm_viewDidLoad() -> () {
-        if self.isKindOfClass(UINavigationController) {
-            let tempGes = (self as! UINavigationController).interactivePopGestureRecognizer
-            tempGes?.enabled = false
-            let arr = tempGes?.valueForKey("_targets")
-            let tar = arr?.firstObject
-            let _targets = tar!!.valueForKey("target");
-            let sel = NSSelectorFromString("handleNavigationTransition:")
-            let pan = UIPanGestureRecognizer.init(target: _targets!, action: sel)
-            tempGes!.view?.addGestureRecognizer(pan)
-        }
+        let tempGes = self.interactivePopGestureRecognizer
+        tempGes?.enabled = false
+        let arr = tempGes?.valueForKey("_targets")
+        let tar = arr?.firstObject
+        let _targets = tar!!.valueForKey("target");
+        let sel = NSSelectorFromString("handleNavigationTransition:")
+        let pan = UIPanGestureRecognizer.init(target: _targets!, action: sel)
+        tempGes!.view?.addGestureRecognizer(pan)
     }
 }
+
